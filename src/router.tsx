dd22d2aa7df73184d24c9b16bdb28c91acdc6fa7
RoyDust <3214026782@qq.com>
2023-07-05 00:54:16 +0800

    提交页面备注 绕过eslint检查

---
+++ src/router.tsx
@@ -0,0 +1,24 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+// ./src/router/index.tsx
+import Home from "@/pages/Home";
+import Demo from "@/pages/Demo";
+
+interface IRouter {
+  path: string;
+  element: JSX.Element;
+  loadData?: (store: any) => any;
+}
+
+const router: Array<IRouter> = [
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/demo",
+    element: <Demo />,
+    loadData: Demo.getInitProps,
+  },
+];
+
+export default router;
